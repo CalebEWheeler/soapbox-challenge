f0ca7b03525b9cceb7496ad92836213cc9916aa3
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-24 12:10:58 -0500

    feat: created a model in trending route that contains a promise that makes a fetch to GIPHY api and returns the top trending gifs. Will need to refactor because I placed my data/keys.js in the wrong directory.

--- app/routes/trending.js
+++ app/routes/trending.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import { api } from '../data/keys';
 
 export default class TrendingRoute extends Route {
+  //creating a model for the route to handle my Promise to GIPHY api.
+  model() {
+    const getTrendingGifs = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${api.key}&limit=25&rating=r`)
+    .then(result => result.json())
+    .then(retrievedGifs => {
+      console.log(retrievedGifs)
+    })
+    .catch(error => console.log(error));
+
+    return getTrendingGifs;
+  }
 }
