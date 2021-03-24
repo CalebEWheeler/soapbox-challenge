44a7c88cdd4bd3cc4f166f253e3b4cd5f18f4336
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-24 15:35:30 -0500

    feat: write three tests for component gif-container/image that test if a 'src' and 'alt' attribute exists with expected values passed, and a test to check if the 'img' has the class 'gif-image'.

--- tests/integration/components/gif-container/image-test.js
+++ tests/integration/components/gif-container/image-test.js
@@ -13,8 +13,11 @@ module('Integration | Component | gif-container/image', function(hooks) {
     this.set('src', "https://media0.giphy.com/media/zaCojXv2S01zy/giphy-downsized.gif?cid=722b3a9fbs8gblxtep0eavs63lgpywu3of8wth60pu1ccdip&rid=giphy-downsized.gif");
 
     await render(hbs`<GifContainer::Image @src={{this.src}}/>`);
+    //will test if the img element has the attribute 'src' and if the passed value of src is the expected set value on line 13.
     assert.dom('img').hasAttribute('src', this.src);
+    //will test if the img element has the attribute 'alt' and if the value if '.gif'.
     assert.dom('img').hasAttribute('alt', '.gif');
+    //will test if the img element has a class named 'gif-image'.
     assert.dom('img').hasClass('gif-image');
     
   });
